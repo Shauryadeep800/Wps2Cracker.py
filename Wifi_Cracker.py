07a749308bd5649c1bcd17cfa83ee742eaf7ce02
Shauryadeep800 <81960829+Shauryadeep800@users.noreply.github.com>
2022-09-09 15:26:28 +0530

    Show Yourself Like A Hacker!
    Note: It is not a Hacking Tool.Just a Prank Only. Show yourself like a Hacker

---
+++ Wifi_Cracker.py
@@ -0,0 +1,95 @@
+#     ............................ CODE IS HERE .......................
+# *colour*
+Black="\033[1;30m"
+Red="\033[1;31m"
+Green="\033[1;32m"
+Yellow="\033[1;33m"
+Blue="\033[1;34m"
+Purple="\033[1;35m"
+Cyan="\033[1;36m"
+White="\033[1;37m"
+print (" ")
+#logo
+print (Green,'''                             
+                ⢠⡾⠃      ⠰⣶⡀  
+               ⢠⡿⠁⣴⠇    ⠸⣦⠈⢿⡄ 
+               ⣾⡇⢸⡏⢰⡇  ⢸⡆⢸⡆⢸⡇ 
+               ⢹⡇⠘⣧⡈⠃⢰⡆⠘⢁⣼⠁⣸⡇ 
+               ⠈⢿⣄⠘⠃ ⢸⡇ ⠘⠁⣰⡟  
+                 ⠙⠃  ⢸⡇  ⠘⠋   
+                     ⢸⡇       
+                     ⢸⡇       
+                     ⠘⠃       
+ ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇ 
+ ⢸⣿⣟⠉⢻⡟⠉⢻⡟⠉⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇ 
+ ⢸⣿⣿⣷⣿⣿⣶⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇ 
+ ⠈⠉⠉⢉⣉⣉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⣉⣉⡉⠉⠉⠁ 
+    ⠉⠉⠉                ⠉⠉⠉    
+''')
+import sys
+from time import sleep
+print (Cyan)
+words = ('''Installing Tools [✓]\nInstalling Server [✓]\nInstalling Scanner [✓]''')
+for char in words:
+    sleep(0.07)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+
+ran = ("3", "5" , "6", "7", "4")
+
+#print ( Green,  '\n FOUND'+(random.coice(ran))+ ' #NETWORKS CONNECTIONS')
+
+print ( Green,  '\n          FOUND 4 NETWORKS CONNECTIONS')
+print (Red, "....................................................................")
+print ("                  [[--- SCANNED CONNECTIONS REPORT ---]]")
+print (Red, "....................................................................")
+# Type effects
+import sys
+from time import sleep
+print (Yellow)
+hack = (
+"""
+=======================================================================================l
+|   IP ADDRESS      |     NAME         |      TYPE      |        PASSWORD              |
+l======================================================================================|
+l 163.0.63.83.3.5         MauryaJi           WPS2                94155363              |
+|                                                                                      |
+l 163.9.33.63.9.6         WI-FI              WPS2                Hanumanji             |
+l                                                                                      |
+l 164.0.37.73.9.0         Redmi 8A           WPS2                geh6je&y              |
+l                                                                                      |
+l 256.3.83.62.2.1         Airtel ™           WPS2                Unknown*              |
+1+ —————————————————--------------------------------------------------------——————————+|
+""")
+for char in hack:
+    sleep(0.01)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+print (Green, 'Code By ShauryaDeep')
+print (Red, "\n Warn: You can't be  Genius by copying my Code...")
+
+#2022-09-01 08:06:56 Thursday                                                                  
+# Connect 
+print (Green, "Choose a network to connect:" 
+, Cyan, "1,2,3 or 4 ")
+inp1 =  ("Choose Your Connection option: "+White)
+inp = input(int())
+
+if int(inp) >= 5:
+    print (White + "Please choose a correct option: 1,2,3 or 4 !"+ Red+ " Option "+ inp + " Not Found in Network List")
+else:
+ import sys
+ from time import sleep
+ print (Cyan)
+ target = ("Target: ")
+ tar_ip = (" 86.55.43.0.0.86.158.0.6")
+ for char in target:
+     sleep(0.10)
+     sys.stdout.write(char)
+     sys.stdout.flush()
+ for char in tar_ip:
+     sleep(0.10)
+     sys.stdout.write(char)
+     sys.stdout.flush()
+  
+ print (Red, "Connection: ", Green, "Success [✓]")
